7cb2766675910ce21bb6af9272d89074eb9026ab
dlbjames <dljames@uw.edu>
2021-04-10 14:33:32 -0700

    Added initial location route

--- app.js
+++ app.js
@@ -2,8 +2,26 @@ const express = require('express')
 const app = express()
 
 
-app.get("/", (request, response) => {
-    response.send("hello")
+app.get("/:location?", (request, response) => {
+    // make a call to the covidActNow api and return the data we need
+    const location = request.params.name ? request.params.name : 'Alabama';
+
+
+    // get api data stuff here 
+
+    // store used data in here
+    res = {
+        "new-cases" : "",
+        "deaths": "",
+        "tot-cases": "",
+        "tot-deaths" : "",
+        "infection-rate": "",
+        "positive-test-rate": "",
+        "percent-vaccinated": "",
+        "vulnerability-level": ""
+    }
+
+    response.send(JSON.stringify(res))
 })
 
 app.listen(process.env.PORT || 3000, () => {
