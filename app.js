a458cd1ad618b619f45b234abe5c1cf1a5cd2240
dlbjames <dljames@uw.edu>
2021-04-10 15:22:07 -0700

    Made get requests for specific counties

--- app.js
+++ app.js
@@ -1,30 +1,38 @@
 const express = require('express')
 const app = express()
+const fetch = require("node-fetch")
+
 require('dotenv').config()
 
-app.get("/:city/:state", (request, response) => {
+app.get("/:city/:state", async (request, response) => {
 
     const city = request.params.city
     const state = request.params.state
     const countyCode = getCountyCode(city, state)
+
     // make a call to the covidActNow api and return the data we need
     const covidURL = `https://api.covidactnow.org/v2/county/${countyCode}.json?apiKey=${process.env.COVIDAPI}`
+    
     // get api data stuff here 
-
+    const countyRequest = await fetch(covidURL);
+    const countyData = await countyRequest.json()
+    //console.log(countyData);
 
     // store used data in here
     const result = {
-        "new-cases" : "",
-        "deaths": "",
-        "tot-cases": "",
-        "tot-deaths" : "",
-        "infection-rate": "",
-        "positive-test-rate": "",
-        "percent-vaccinated": "",
-        "vulnerability-level": ""
+        "new-cases" : countyData.actuals.newCases,
+        "new-deaths": countyData.actuals.newDeaths,
+        "tot-cases": countyData.actuals.cases,
+        "tot-deaths" : countyData.actuals.deaths,
+        "infection-rate": countyData.metrics.infectionRate,
+        "positive-test-rate": countyData.metrics.testPositivityRatio,
+        "percent-vaccinated": countyData.metrics.vaccinationsCompletedRatio,
+        "vaccines-administered": countyData.actuals.vaccinesAdministered,
+        "vaccines-completed": countyData.actuals.vaccinationsCompleted,
+        "vulnerability-level": countyData.riskLevels.overall,
     }
 
-    response.send(result)
+    response.send(JSON.stringify(result))
 })
 
 app.listen(process.env.PORT || 5000, () => {
