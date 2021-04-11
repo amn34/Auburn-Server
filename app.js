ae76c6896f38f20084cf4ed375bcbbd1bedbba67
Anthony <nguyenanthony80@gmail.com>
2021-04-10 22:59:30 -0700

    added location

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.get("/", (request, response) => {
 app.get("/:state", async (request, response) => {
     const state = request.params.state.toUpperCase()
     const covidStateURL = `https://api.covidactnow.org/v2/state/${state}.json?apiKey=${process.env.COVIDAPI}`
-    const covidData = await getCovidData(covidStateURL).catch(function() {})
+    const covidData = await getCovidData(covidStateURL, state).catch(function() {})
     response.send(covidData);
 })
 
@@ -25,7 +25,7 @@ app.get("/:state/:city", async (request, response) => {
     const state = request.params.state
     const countyCode = await getCountyCode(city, state).catch(function() {})
     const covidURL = `https://api.covidactnow.org/v2/county/${countyCode}.json?apiKey=${process.env.COVIDAPI}`
-    const covidData = await getCovidData(covidURL).catch(function() {})
+    const covidData = await getCovidData(covidURL, state, city).catch(function() {})
     response.send(covidData)
 })
 
@@ -39,9 +39,17 @@ app.listen(process.env.PORT || 8080, () => {
  * @param { JSON } data - The data which holds all covid statistics from covidactnow.org
  * @returns A JSON object containing the statistics in use for the applications
  */
-async function getCovidData(covidURL) {
+async function getCovidData(covidURL, state, city) {
     const dataRequest = await fetch(covidURL).catch(function() {})
     const data = await dataRequest.json().catch(function() {})
+
+    let location;
+    if(city) {
+        location = formatCity(city) + ", " + state.toUpperCase()
+    } else {
+        location = state.toUpperCase()
+    }
+
 
     return {
         "new-cases": data.actuals.newCases,
@@ -54,6 +62,7 @@ async function getCovidData(covidURL) {
         "vaccines-administered": data.actuals.vaccinesAdministered,
         "vaccines-completed": data.actuals.vaccinationsCompleted,
         "vulnerability-level": data.riskLevels.overall,
+        "location": location
     }
 }
 
@@ -68,4 +77,8 @@ async function getCountyCode(cityName, stateName) {
     const data = await response.json().catch(function() {})
 
     return data.results[0].county_fips || "01081"; //default auburn,alabama county code
+}
+
+function formatCity(city) {
+    return city.charAt(0).toUpperCase() + city.toLowerCase().slice(1)
 }
