118b98fb963367356877ed7550cba05fdbdc20f9
dlbjames <dljames@uw.edu>
2021-04-10 20:44:58 -0700

    Add files for running on google cloud

--- app.js
+++ app.js
@@ -6,24 +6,28 @@ const cities = require('all-the-cities')
 
 require('dotenv').config()
 
+app.get("/", (request, response) => {
+    response.send("Welcome to the CovidStats API");
+})
+
 app.get("/:state", async (request, response) => {
     const state = request.params.state.toUpperCase()
     const covidStateURL = `https://api.covidactnow.org/v2/state/${state}.json?apiKey=${process.env.COVIDAPI}`
-    const covidData = await getCovidData(covidStateURL);
+    const covidData = await getCovidData(covidStateURL).catch(function() {})
     response.send(covidData);
 })
 
 app.get("/:state/:city", async (request, response) => {
     const city = request.params.city
     const state = request.params.state
-    const countyCode = await getCountyCode(city, state)
+    const countyCode = await getCountyCode(city, state).catch(function() {})
     const covidURL = `https://api.covidactnow.org/v2/county/${countyCode}.json?apiKey=${process.env.COVIDAPI}`
-    const covidData = await getCovidData(covidURL)
+    const covidData = await getCovidData(covidURL).catch(function() {})
     response.send(covidData)
 })
 
-app.listen(process.env.PORT || 5000, () => {
-    console.log('Server is running on ' + (process.env.PORT || 5000))
+app.listen(process.env.PORT || 8080, () => {
+    console.log('Server is running on ' + (process.env.PORT || 8080))
 })
 
 /**
@@ -33,8 +37,8 @@ app.listen(process.env.PORT || 5000, () => {
  * @returns A JSON object containing the statistics in use for the applications
  */
 async function getCovidData(covidURL) {
-    const dataRequest = await fetch(covidURL)
-    const data = await dataRequest.json()
+    const dataRequest = await fetch(covidURL).catch(function() {})
+    const data = await dataRequest.json().catch(function() {})
 
     return {
         "new-cases": data.actuals.newCases,
@@ -57,8 +61,8 @@ async function getCountyCode(cityName, stateName) {
     )
     const latlong = matches[0].loc.coordinates
     const countyCodeURL = `https://geo.fcc.gov/api/census/area?lat=${latlong[1]}&lon=${latlong[0]}&format=json`
-    const response = await fetch(countyCodeURL)
-    const data = await response.json()
+    const response = await fetch(countyCodeURL).catch(function() {})
+    const data = await response.json().catch(function() {})
 
     return data.results[0].county_fips || "01081"; //default auburn,alabama county code
 }
